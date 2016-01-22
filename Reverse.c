c4410b0a1025a43262690b68e0ba0cbe7890214b
Sugan <nsugan96@gmail.com>
2016-01-22 13:18:49 +0530

    Update Reverse.c

--- Reverse.c
+++ Reverse.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 void main()
 {
-int n,r,s=0;
-printf("\nEnter the number to be reversed:");
-scanf("%d",&n);
+  int n,r,s=0;
+  printf("\nEnter the number to be reversed:");
+  scanf("%d",&n);
 while(n>0)
-{
-r=n%10;
-s=(s*10)+r;
-n=n/10;
-}
-printf("\nThe reversed number is %d",s);
+  {
+    r=n%10;
+    s=(s*10)+r;
+    n=n/10;
+   }
+  printf("\nThe reversed number is %d",s);
 }
